43a1ce92e2bccfff480d44a0c48a3a2cf22ecc0f
CD Cabrera <ccabrera@redhat.com>
2019-09-17 12:52:40 -0400

    DEV branch push test

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -13,18 +13,18 @@
     exit 0;
   fi
 
-  if [[ "${TRAVIS_BRANCH}" = "master" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Build"* ]]; then
+  if [[ "${TRAVIS_BRANCH}" = "dev" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Build"* ]]; then
     printf "${YELLOW}PUSHING Quipudocs build...${NOCOLOR}\n"
 
     git config --global user.name "travis@travis-ci.org"
     git config --global user.email "CI Build"
     git remote add ssh-origin ${REPO}
-    git checkout -b test
+    git checkout -b dev-test
 
     git add -f ./dist/*
     git add -f ./docs/*
     git commit -m "chore(build): deploy"
-    git push ssh-origin test --quiet
+    git push ssh-origin dev-test --quiet
 
     printf "${GREEN}COMPLETED Quipudocs build${NOCOLOR}\n"
     exit 0;
