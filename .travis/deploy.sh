da918158c860b95e42d786e6afa644d78bb83781
CD Cabrera <ccabrera@redhat.com>
2019-09-18 10:05:04 -0400

    DEV deploy update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,9 +4,17 @@
 # main()
 #
 {
+  set -e
+  set -x
+
   GREEN="\e[32m"
   YELLOW="\e[33m"
   NOCOLOR="\e[39m"
+
+  if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+    echo -e "${YELLOW}Exiting early, pull request${NOCOLOR}"
+    exit 0;
+  fi
 
   if [[ $REPO != *"$TRAVIS_REPO_SLUG"* ]]; then
     echo -e "${YELLOW}Exiting early, not master repository${NOCOLOR}"
@@ -14,7 +22,18 @@
   fi
 
   if [[ "${TRAVIS_BRANCH}" = "dev" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Build"* ]]; then
-    printf "${YELLOW}PUSHING Quipudocs build...${NOCOLOR}\n"
+    set +x
+    openssl aes-256-cbc \
+            -K `env | grep 'encrypted_.*_key' | cut -f2 -d '='` \
+            -iv `env | grep 'encrypted_.*_iv' | cut -f2 -d '='` \
+            -in .travis/deploy_key.enc -out .travis/deploy_key -d
+    set -x
+
+    chmod 600 .travis/deploy_key
+    eval `ssh-agent -s`
+    ssh-add .travis/deploy_key
+
+    echo -e "${YELLOW}PUSHING Quipudocs build...${NOCOLOR}"
 
     git config --global user.name "travis@travis-ci.org"
     git config --global user.email "CI Build"
@@ -26,7 +45,7 @@
     git commit -m "chore(build): deploy"
     git push ssh-origin dev-test --quiet
 
-    printf "${GREEN}COMPLETED Quipudocs build${NOCOLOR}\n"
+    echo -e "${GREEN}COMPLETED Quipudocs build${NOCOLOR}"
     exit 0;
   fi
 
