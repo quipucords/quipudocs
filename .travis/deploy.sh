beabc40432a8fb420f153af33294fec857f4c0e4
CD Cabrera <ccabrera@redhat.com>
2019-09-18 12:19:23 -0400

    DEV env variables

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -21,7 +21,7 @@
     exit 0;
   fi
 
-  if [[ "${TRAVIS_BRANCH}" = "dev" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Build"* ]]; then
+  if [[ "${TRAVIS_BRANCH}" = "${BRANCH}" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Build"* ]]; then
     set +x
     openssl aes-256-cbc \
             -K `env | grep 'encrypted_.*_key' | cut -f2 -d '='` \
@@ -35,15 +35,15 @@
 
     echo -e "${YELLOW}PUSHING Quipudocs build...${NOCOLOR}"
 
-    git config --global user.name "travis@travis-ci.org"
-    git config --global user.email "CI Build"
+    git config --global user.name ${USER}
+    git config --global user.email ${EMAIL}
     git remote add ssh-origin ${REPO}
-    git checkout -b dev-test
+    git checkout ${BRANCH}
 
     git add -f ./dist/*
     git add -f ./docs/*
     git commit -m "chore(build): deploy"
-    git push ssh-origin dev-test --quiet
+    git push ssh-origin ${BRANCH} --quiet
 
     echo -e "${GREEN}COMPLETED Quipudocs build${NOCOLOR}"
     exit 0;
