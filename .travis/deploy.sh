1d94516d3758dba54956294225d5241ee813658f
CD Cabrera <ccabrera@redhat.com>
2019-09-20 17:08:47 -0400

    feat(build): issues/54,69 dist and GitHub pages (#82)
    * package updates
    * basic tests for dist directory, confirm build
    * deploy scripts
    * build and testing script updates
    * align ea and local dev naming
    * docs symlinks
    * base files dist and docs
    * asciidoc GitHub pages layout and base output
    * test deploy commit message token

---
+++ .travis/deploy.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+#
+# main()
+#
+{
+  set -e
+  set -x
+
+  GREEN="\e[32m"
+  YELLOW="\e[33m"
+  NOCOLOR="\e[39m"
+
+  if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+    echo -e "${YELLOW}Exiting early, pull request${NOCOLOR}"
+    exit 0;
+  fi
+
+  if [[ $REPO != *"$TRAVIS_REPO_SLUG"* ]]; then
+    echo -e "${YELLOW}Exiting early, not master repository${NOCOLOR}"
+    exit 0;
+  fi
+
+  if [[ "[DEPLOY]" != *"$TRAVIS_COMMIT_MESSAGE"* ]]]; then
+    echo -e "${YELLOW}Exiting early, not a deployment${NOCOLOR}"
+    exit 0;
+  fi
+
+  if [[ "${TRAVIS_BRANCH}" = "${BRANCH}" ]] && [[ $TRAVIS_BUILD_STAGE_NAME == *"Build"* ]]; then
+    set +x
+    openssl aes-256-cbc \
+            -K `env | grep 'encrypted_.*_key' | cut -f2 -d '='` \
+            -iv `env | grep 'encrypted_.*_iv' | cut -f2 -d '='` \
+            -in .travis/deploy_key.enc -out .travis/deploy_key -d
+    set -x
+
+    chmod 600 .travis/deploy_key
+    eval `ssh-agent -s`
+    ssh-add .travis/deploy_key
+
+    echo -e "${YELLOW}PUSHING Quipudocs build...${NOCOLOR}"
+
+    git config --global user.name ${USER}
+    git config --global user.email ${EMAIL}
+    git remote add ssh-origin ${REPO}
+    git checkout ${BRANCH}
+
+    git add -f ./dist/*
+    git add -f ./docs/*
+    git commit -m "chore(build): deploy" -m "[skip ci]"
+    git push ssh-origin ${BRANCH} --quiet
+
+    echo -e "${GREEN}COMPLETED Quipudocs build${NOCOLOR}"
+    exit 0;
+  fi
+
+  echo -e "${YELLOW}Exiting, not ${BRANCH} branch or build stage${NOCOLOR}"
+  exit 0;
+}
