9e4296afefdd3168c74efe39ae3dea7399fb2b48
CD Cabrera <ccabrera@redhat.com>
2019-09-20 16:41:49 -0400

    DEV build GitHub pages clean up

--- scripts/build.gh.sh
+++ scripts/build.gh.sh
@@ -7,9 +7,10 @@ parseAsciidoc()
 {
   local DIR=$1
   local OUTPUT=$2
-  local ADOC="${3:-master.adoc}"
+  local TOC="${3}"
+  local ADOC="${4:-master.adoc}"
 
-  asciidoctor -v --failure-level error -S safe -d book -B $DIR -o $OUTPUT "$DIR/$ADOC" -a toc=left
+  asciidoctor -v --failure-level error -S safe -d book -B $DIR -o $OUTPUT "$DIR/$ADOC" -a toc="$TOC"
 }
 #
 #
@@ -17,8 +18,9 @@ parseAsciidoc()
 #
 ghPages()
 {
-  parseAsciidoc ./src/community/titles/install/qpc ./docs/install.html
-  parseAsciidoc ./src/community/titles/user/qpc ./docs/user.html
+  parseAsciidoc ./src/community/titles/gh_pages ./docs/index.html
+  parseAsciidoc ./src/community/titles/install/qpc ./docs/install.html left
+  parseAsciidoc ./src/community/titles/user/qpc ./docs/user.html left
 }
 #
 #
