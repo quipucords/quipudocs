79d75d3d1057b7664e92cbc0fdc1324b81c50447
CD Cabrera <ccabrera@redhat.com>
2019-09-20 21:23:00 -0400

    DEV clean up

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -21,7 +21,7 @@
     exit 0;
   fi
 
-  if [[ "[DEPLOY]" != *"$TRAVIS_COMMIT_MESSAGE"* ]]; then
+  if [[ ! "$TRAVIS_COMMIT_MESSAGE" =~ "[DEPLOY]" ]]; then
     echo -e "${YELLOW}Exiting early, not a deployment${NOCOLOR}"
     exit 0;
   fi
