586f034c114b49bc82826900ff33e1e09ff7589f
CD Cabrera <ccabrera@redhat.com>
2019-09-20 22:11:43 -0400

    DEV dist docs integration test update

--- tests/dist.test.js
+++ tests/dist.test.js
@@ -2,20 +2,36 @@ const { execSync } = require('child_process');
 const Quipudocs = require('../dist');
 
 describe('Distribution', () => {
-  const outputDir = 'dist';
+  const outputDistDir = 'dist';
+  const outputDocsDir = 'docs';
 
   it('should have specific defined properties for JS consumption', () => {
     expect(Object.keys(Quipudocs)).toMatchSnapshot('exported output');
   });
 
-  it('should match a specific file output', () => {
-    const output = execSync(`find ./${outputDir} -type f -print0 | xargs -0`);
+  it('should match a specific dist file output', () => {
+    const output = execSync(`find ./${outputDistDir} -type f -print0 | xargs -0`);
 
     const replacedGeneratedFilesMinsHash = output
       .toString()
       .replace(/\s+|\n+|\r+/g, '')
-      .replace(new RegExp(`./${outputDir}`, 'gi'), `~./${outputDir}`)
-      .replace(new RegExp(`~./${outputDir}/.DS_Store`, 'gi'), '')
+      .replace(new RegExp(`./${outputDistDir}`, 'gi'), `~./${outputDistDir}`)
+      .replace(new RegExp(`~./${outputDistDir}/.DS_Store`, 'gi'), '')
+      .replace(/\.([a-z0-9]+)\./gi, '*')
+      .split('~')
+      .sort();
+
+    expect(replacedGeneratedFilesMinsHash).toMatchSnapshot();
+  });
+
+  it('should match a specific docs file output', () => {
+    const output = execSync(`find ./${outputDocsDir} -type f -print0 | xargs -0`);
+
+    const replacedGeneratedFilesMinsHash = output
+      .toString()
+      .replace(/\s+|\n+|\r+/g, '')
+      .replace(new RegExp(`./${outputDocsDir}`, 'gi'), `~./${outputDocsDir}`)
+      .replace(new RegExp(`~./${outputDocsDir}/.DS_Store`, 'gi'), '')
       .replace(/\.([a-z0-9]+)\./gi, '*')
       .split('~')
       .sort();
